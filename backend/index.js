5d11f4f2183e28119a464cb660add7f7033811bd
Ahmad Halabi <halabi909@gmail.com>
2024-02-16 06:04:51 +0300

    update-cors

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost/5137",
+    origin: "https://mern-client-whfs.onrender.com/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
